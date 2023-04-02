06968d4dce3e9914b138b1d3958665262c723c10
Paul <47425463+PaulBykov@users.noreply.github.com>
2023-04-02 13:21:35 +0300

    Add files via upload

--- js/tour.js
+++ js/tour.js
@@ -1,17 +1,19 @@
 fetch('../xml/materials.xml')
   .then(res => res.text())
   .then(xmlString => {
-    const Documents = ['shri.html', 'egypt.html', 'turkey.html', 'indonesia.html', 'bolgaria.html', 'japan.html', 'portugal.html', 'brazil.html', 'alps.html', 'canada.html', 'cruise.html']
+    const Documents = ['shri.html', 'egypt.html', 'turkey.html', 'indonesia.html', 'bolgaria.html', 'japan.html', 'portugal.html', 'costa-rica.html', 'brazil.html', 'alps.html', 'canada.html', 'cruise.html']
     const parser = new DOMParser();
     const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
     const descs = xmlDoc.getElementsByTagName('DESCRIPTION');
     const cities = xmlDoc.getElementsByTagName('CITY');
     const hotels = xmlDoc.getElementsByTagName('HOTEL');
-    for(let i = 0; i < 12; i++){
+    const date = xmlDoc.getElementsByTagName('DATE');
+    for(let i = 0; i <= 12; i++){
       if(document.location.pathname == '/tours/' + Documents[i]){
         document.querySelector('.city').innerHTML = cities[i].textContent;
         document.querySelector('.desc').innerHTML = descs[i].textContent;
         document.querySelector('.hotel').innerHTML = hotels[i].textContent;
+        document.querySelector('.date').innerHTML = cities[i].textContent;
       }
     }
   })
