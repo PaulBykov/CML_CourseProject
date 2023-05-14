35944a1188bfb77a7751eeddf60273077ccf0086
Paul <47425463+PaulBykov@users.noreply.github.com>
2023-05-14 20:02:13 +0300

    Add files via upload

--- js/smothMove.js
+++ js/smothMove.js
@@ -1,12 +1,10 @@
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // отменяем стандартное поведение ссылки
+        e.preventDefault();
         
-        // получаем id элемента, к которому нужно прокрутить
         const target = document.querySelector(this.getAttribute('href'));
         
-        // скроллим до элемента с плавностью
         target.scrollIntoView({
             behavior: 'smooth'
         });
